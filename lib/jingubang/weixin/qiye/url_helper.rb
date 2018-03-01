51ebce4018bb353a0c098557474a646aee079524
FENG Zhichao <flankerfc@gmail.com>
2018-03-01 16:45:42 +0800

    Helper to generate third party oauth url

--- lib/jingubang/weixin/qiye/url_helper.rb
+++ lib/jingubang/weixin/qiye/url_helper.rb
@@ -9,6 +9,10 @@ module Jingubang::Weixin::Qiye
         "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{corpid}&redirect_uri=#{redirect_uri}&response_type=code&scope=snsapi_base&agentid=#{agentid}&state=#{state}#wechat_redirect"
       end
 
+      def third_party_user_oauth_path(redirect_uri, agentid: nil, state: '', user_type: 'member')
+        "https://open.work.weixin.qq.com/wwopen/sso/3rd_qrConnect?appid=#{agentid}&redirect_uri=#{redirect_uri}&state=#{state}&usertype=#{user_type}"
+      end
+
     end
   end
 end
