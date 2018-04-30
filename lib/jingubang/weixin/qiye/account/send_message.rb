8320b630e0bc5f1a3f8c573ee9fe2071c12ddf50
FENG Zhichao <flankerfc@gmail.com>
2018-05-01 00:33:48 +0800

    API to send new type message (weixin qiye)

--- lib/jingubang/weixin/qiye/account/send_message.rb
+++ lib/jingubang/weixin/qiye/account/send_message.rb
@@ -17,6 +17,25 @@ module Jingubang::Weixin::Qiye::Account
       response = fire_request path, body
     end
 
+    def send_news_message userids: [], title: nil, description: nil, url: nil, button_text: '详情', image_url: nil
+      path = "/cgi-bin/message/send?access_token=#{refreshed_access_token}"
+      body = {
+        touser: userids_param(userids),
+        msgtype: 'news',
+        agentid: agentid,
+        news: {
+          articles: [{
+            title: title,
+            description: description,
+            url: url,
+            picurl: image_url,
+            btntxt: button_text
+          }]
+        }
+      }
+      response = fire_request path, body
+    end
+
     private
 
     def userids_param userids
